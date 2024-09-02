9ce8703c601bfdfd7e849ff0da3ee3802131ae8a
Aidan Tay <silver14211@gmail.com>
2024-09-03 06:19:42 +0800

    generated 2015 files

---
+++ 2015/19/1.py
@@ -0,0 +1,10 @@
+from collections import deque, defaultdict, Counter
+import re
+from functools import cache
+import itertools
+from heapq import heappop, heappush
+import math
+import numpy
+
+with open("data.txt") as f:
+    data = f.read().splitlines()
