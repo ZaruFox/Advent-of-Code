6b2cc33f37f59ea250917e10bc67a117b95ddba7
Aidan Tay <silver14211@gmail.com>
2024-12-06 22:37:13 +0800

    refactored day 5 and day 6

--- 2024/6/1.py
+++ 2024/6/1.py
@@ -14,16 +14,21 @@ for y, row in enumerate(data):
             curX = x
             curY = y
 
-directions = ((0,-1), (1,0), (0,1), (-1, 0))
+DIRECTIONS = ((0,-1), (1,0), (0,1), (-1, 0))
+n, m = len(data), len(data[0])
 curDir = 0
 visited = set()
 
-while 0 <= curY < len(data) and 0 <= curX < len(data[0]):
-    dx, dy = directions[curDir]
+while True:
+    dx, dy = DIRECTIONS[curDir]
     visited.add((curX, curY))
     
-    if (0 <= curY+dy < len(data) and 0 <= curX+dx < len(data[0])) and data[curY+dy][curX+dx] == "#":
-            curDir = (curDir+1)%4
+    if not (0 <= curY+dy < n and 0 <= curX+dx < m):
+         break
+    
+    elif data[curY+dy][curX+dx] == "#":
+            curDir = (curDir + 1) % 4
+
     else:
         curX += dx
         curY += dy
--- 2024/6/2.py
+++ 2024/6/2.py
@@ -5,36 +5,59 @@ import itertools
 from heapq import heappop, heappush
 import math
 
+DIRECTIONS = ((0,-1), (1,0), (0,1), (-1, 0))
+
+def checkLoop(curX, curY, newX, newY): 
+    curDir = 0
+    visited = set()
+    
+    while True:
+        dx, dy = DIRECTIONS[curDir]
+        if (curX, curY, curDir) in visited:
+            return True
+        visited.add((curX, curY, curDir))
+
+        if not (0 <= curY+dy < len(data) and 0 <= curX+dx < len(data[0])):
+            return False
+    
+        elif (data[curY+dy][curX+dx] == "#" or (curX+dx, curY+dy) == (newX, newY)):
+            curDir = (curDir+1)%4
+
+        else:
+            curX += dx
+            curY += dy
+
+
 with open("data.txt") as f:
     data = f.read().splitlines()
 
 for y, row in enumerate(data):
     for x, char in enumerate(row):
         if char == "^":
-            curX = x
-            curY = y
+            startX = x
+            startY = y
 
-def checkLoop(curX, curY, newX, newY): 
-    directions = ((0,-1), (1,0), (0,1), (-1, 0))
-    curDir = 0
-    visited = set()
-    
-    while 0 <= curY < len(data) and 0 <= curX < len(data[0]):
-        dx, dy = directions[curDir]
-        if (curX, curY, curDir) in visited:
-            return True
-        visited.add((curX, curY, curDir))
-    
-        if (0 <= curY+dy < len(data) and 0 <= curX+dx < len(data[0])) and (data[curY+dy][curX+dx] == "#" or (curX+dx, curY+dy) == (newX, newY)):
-            curDir = (curDir+1)%4
-        else:
-            curX += dx
-            curY += dy
+n, m = len(data), len(data[0])
+curDir = 0
+visited = set()
+curX, curY = startX, startY
 
-    return False
+while True:
+    dx, dy = DIRECTIONS[curDir]
+    visited.add((curX, curY))
+    
+    if not (0 <= curY+dy < n and 0 <= curX+dx < m):
+         break
+    
+    elif data[curY+dy][curX+dx] == "#":
+            curDir = (curDir + 1) % 4
+
+    else:
+        curX += dx
+        curY += dy
+
 res = 0
-for y in range(len(data)):
-    for x in range(len(data[0])):
-        if data[y][x] == "." and checkLoop(curX, curY, x, y):
-            res += 1
+for x, y in visited:
+    if checkLoop(startX, startY, x, y):
+        res += 1
 print(res)
