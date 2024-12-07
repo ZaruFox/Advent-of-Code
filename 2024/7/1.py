b6e4e6fdc57aa3b7ec99a7b5372b8ee6b8c3b6c9
Aidan Tay <silver14211@gmail.com>
2024-12-07 15:17:30 +0800

    day 6

--- 2024/7/1.py
+++ 2024/7/1.py
@@ -6,5 +6,22 @@ from heapq import heappop, heappush
 import math
 import numpy
 
+def isValid(target, i, nums):
+    if i == -1:
+        return target == 0
+
+    return isValid(target-nums[i], i-1, nums) or (isValid(target // nums[i], i-1, nums) if target % nums[i] == 0 else False)
+
 with open("data.txt") as f:
     data = f.read().splitlines()
+
+res = 0
+for row in data:
+    target, nums = row.split(": ")
+    target = int(target)
+    nums = [int(x) for x in nums.split()]
+
+    if isValid(target, len(nums)-1, nums):
+        res += target
+
+print(res)
---
+++ 2024/7/2.py
@@ -0,0 +1,31 @@
+from collections import deque, defaultdict, Counter
+import re
+from functools import cache
+import itertools
+from heapq import heappop, heappush
+import math
+import numpy
+
+def isValid(target, i, nums, current=0):
+    if i == len(nums):
+        return current == target
+    
+    return isValid(target, i+1, nums, current+nums[i]) or \
+    isValid(target, i+1, nums, current*nums[i]) or \
+    isValid(target, i+1, nums, int(str(current) + str(nums[i])))
+
+
+
+with open("data.txt") as f:
+    data = f.read().splitlines()
+
+res = 0
+for row in data:
+    target, nums = row.split(": ")
+    target = int(target)
+    nums = [int(x) for x in nums.split()]
+
+    if isValid(target, 0, nums):
+        res += target
+
+print(res)
