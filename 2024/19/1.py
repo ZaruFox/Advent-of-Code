c84dc3e632bf7135f62f89b1309730728caf4d1f
Aidan <silver14211@gmail.com>
2024-12-19 15:57:41 +0800

    day 19

--- 2024/19/1.py
+++ 2024/19/1.py
@@ -7,4 +7,20 @@ import math
 import numpy
 
 with open("data.txt") as f:
-    data = f.read().splitlines()
+    towels, cases = f.read().split("\n\n")
+    cases = cases.splitlines()
+    towels = towels.split(", ")
+
+@cache
+def solve(case, i):
+    if i == len(case):
+        return True
+    
+    res = False
+    for towel in towels:
+        if towel == case[i:len(towel)+i]:
+            res |= solve(case, i + len(towel))
+
+    return res
+
+print(sum(solve(case, 0) for case in cases))
---
+++ 2024/19/2.py
@@ -0,0 +1,26 @@
+from collections import deque, defaultdict, Counter
+import re
+from functools import cache
+import itertools
+from heapq import heappop, heappush
+import math
+import numpy
+
+with open("data.txt") as f:
+    towels, cases = f.read().split("\n\n")
+    cases = cases.splitlines()
+    towels = towels.split(", ")
+
+@cache
+def solve(case, i):
+    if i == len(case):
+        return 1
+    
+    res = 0
+    for towel in towels:
+        if towel == case[i:len(towel)+i]:
+            res += solve(case, i + len(towel))
+
+    return res
+
+print(sum(solve(case, 0) for case in cases))
